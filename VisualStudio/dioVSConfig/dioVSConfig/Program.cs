953635df46d14e1073c9f8fbe3b3e941269df029
AJV SISTEMAS <ajvsistemas@gmail.com>
2022-01-05 13:23:09 -0300

    adicionado projeto de teste 1

---
+++ VisualStudio/dioVSConfig/dioVSConfig/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace dioVSConfig
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            // concatanando string
+            String a = "Olá ";
+            String b = "Mundo!";
+            Console.WriteLine(a + b);
+        }
+    }
+}
